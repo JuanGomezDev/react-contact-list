ef6eca4339bbf6d66ff69df1b23632fda4212208
Juan Sebastian Gomez Ayala <j.gomezayala@globant.com>
2024-03-06 14:59:12 -0500

    crear api.js con baseURL y endpoint getUsers de page 1 por defecto, consumir api en componente ContactList

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,17 +1,31 @@
-import PropTypes from 'prop-types';
 import CardList from "./CardList";
 import HeaderDivider from "./HeaderDivider";
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { getUsers } from '../services/api';
 
 
-export default function ContactList({contacts}) {
+export default function ContactList() {
+    const [contacts, setContacts] = useState([]);
+
+    // Consumar api, obtener usuarios
+    useEffect(() => {
+        const fetchContacts = async () => {
+            try {
+                const res = await getUsers();
+                setContacts(res.data.data);
+            } catch (error) {
+                console.log('Error: ', error);
+            }
+        }
+
+        fetchContacts();
+    }, [])
+    
     return (
         <section>
             <HeaderDivider title="Contact List" />
             <CardList contactsList={contacts} />
         </section>
     )
-}
-
-ContactList.propTypes = {
-    contacts: PropTypes.array.isRequired,
 }
